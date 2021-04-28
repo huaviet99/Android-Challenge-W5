794d215728dc422a5e3fbcaa7696697e11074ed1
Viet Hua <huaviet999@gmail.com>
2021-04-28 11:36:54 +0700

    switch from activites to fragment

---
+++ app/src/main/java/com/thesis/android_challenge_w5/presentation/restaurant_list/RestaurantListViewModel.kt
@@ -0,0 +1,17 @@
+package com.thesis.android_challenge_w5.presentation.restaurant_list
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.thesis.android_challenge_w5.data.getRestaurantList
+import com.thesis.android_challenge_w5.model.Restaurant
+
+class RestaurantListViewModel : ViewModel(){
+
+    private val restaurantList = MutableLiveData<List<Restaurant>>()
+
+    fun fetchRestaurantList(): LiveData<List<Restaurant>>{
+        restaurantList.value = getRestaurantList()
+        return restaurantList
+    }
+}
